7c0d6585e5b0a934172b80b146a0484f409c9c2e
AhmedAElrefae <besoaboadel@gmail.com>
2025-02-26 12:03:18 +0200

    chore(ui): install shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from "path";
 
 export default defineConfig({
     server: {
@@ -39,7 +40,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': '/resources/js/src',
+            "@": path.resolve(__dirname, "resources/js"),
         },
     },
 });
