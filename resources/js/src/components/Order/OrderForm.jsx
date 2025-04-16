0a4ed63a711578e6aa9f7f1433d35a8df10ec77a
AhmedAElrefae <besoaboadel@gmail.com>
2025-04-16 14:46:09 +0200

    refactor(ui): remove unneeded dependency

--- resources/js/src/components/Order/OrderForm.jsx
+++ resources/js/src/components/Order/OrderForm.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useForm, useFieldArray, Controller} from 'react-hook-form';
 import {Button} from '@/components/ui/button';
